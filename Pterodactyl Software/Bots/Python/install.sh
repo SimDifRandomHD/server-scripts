65ac83d38d33f61617b198dcd0dda6d8b1fa2ed6
LochStudios <lachlan@lochstudios.com>
2020-05-20 18:48:28 +1000

    Delete install.sh

--- Pterodactyl Software/Bots/Python/install.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get update -y
-sudo apt-get install git libopus-dev libffi-dev libsodium-dev ffmpeg -y
-sudo apt-get install build-essential libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl -y
-sudo apt-get install python3-pip -y
-git clone https://github.com/Just-Some-Bots/MusicBot.git -b master
-sudo -H python3.5 -m pip install --upgrade pip
-sudo -H python3.5 -m pip install --upgrade -r requirements.txt
