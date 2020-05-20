03fd999eb7f9bdf6cd749f00ecf51587fba4c0bd
LochStudios <lachlan@lochstudios.com>
2020-05-20 18:08:54 +1000

    Update install.sh

--- Pterodactyl Software/Bots/Python/install.sh
+++ Pterodactyl Software/Bots/Python/install.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 sudo apt-get update -y
 sudo apt-get install git libopus-dev libffi-dev libsodium-dev ffmpeg -y
 sudo apt-get install build-essential libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl -y
