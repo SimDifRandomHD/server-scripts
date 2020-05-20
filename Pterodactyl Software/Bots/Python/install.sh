5bafe50ece64331cda94b4d6fe9402e219b4c195
LochStudios <lachlan@lochstudios.com>
2020-05-20 18:05:56 +1000

    Update install.sh

--- Pterodactyl Software/Bots/Python/install.sh
+++ Pterodactyl Software/Bots/Python/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-apt-get update -y
-apt-get install git libopus-dev libffi-dev libsodium-dev ffmpeg -y
-apt-get install build-essential libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl -y
-apt-get install python3-pip -y
+sudo apt-get update -y
+sudo apt-get install git libopus-dev libffi-dev libsodium-dev ffmpeg -y
+sudo apt-get install build-essential libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl -y
+sudo apt-get install python3-pip -y
 git clone https://github.com/Just-Some-Bots/MusicBot.git -b master
 sudo -H python3.5 -m pip install --upgrade pip
 sudo -H python3.5 -m pip install --upgrade -r requirements.txt
