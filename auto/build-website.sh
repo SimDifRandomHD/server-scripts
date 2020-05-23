10c48de12d757731e7e0116ac7b0176dc44e8560
LochStudios <lachlan@lochstudios.com>
2020-05-24 09:42:21 +1000

    Create build-website.sh

---
+++ auto/build-website.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install software-properties-common python-software-properties build-essential zip unzip apache2 php libapache2-mod-php php-mcrypt php-mysql mysql-server -y
