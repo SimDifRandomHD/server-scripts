b7103f66a57710da38ebb78cb4e5d978de728aba
LochStudios <lachlan@lochstudios.com>
2021-04-27 17:48:56 +1000

    Create Teamspeak 3 Hosting Client Install (ubuntu).sh

---
+++ other/Teamspeak 3 Hosting Client Install (ubuntu).sh
@@ -0,0 +1,100 @@
+#Update the system and do a quick reboot.
+sudo apt-get update; sudo apt-get upgrade -y; sudo reboot now
+#Make sure you have these installed, if not run this:
+sudo apt-get install nano wget tar perl bzip2 -y
+#Download the new server files and move to that folder
+wget http://dl.4players.de/ts/releases/3.4.0/teamspeak3-server_linux_amd64-3.4.0.tar.bz2 -P /home/temp; cd /home/temp
+#Extract the archive
+tar xjf teamspeak3-server_linux_amd64*.tar.bz2
+#Create a new user with your desired name.
+sudo adduser --disabled-login teamspeak
+#Move the extracted files to the teamspeak user's home directory then remove the extracted folder and downloaded archive.
+cd teamspeak3-server_linux_amd64 && mv * /home/teamspeak && cd .. && rm -rf teamspeak3*
+#Accept the license agreement:
+touch /home/teamspeak/.ts3server_license_accepted
+#Change ownership of the TeamSpeak 3 server files
+sudo chown -R teamspeak:teamspeak /home/teamspeak
+#Make sure all the ports are open on the server's firewall
+iptables -A INPUT -p udp --dport 9987 -j ACCEPT
+iptables -A INPUT -p udp --sport 9987 -j ACCEPT
+iptables -A INPUT -p tcp --dport 30033 -j ACCEPT
+iptables -A INPUT -p tcp --sport 30033 -j ACCEPT
+iptables -A INPUT -p tcp --dport 10011 -j ACCEPT
+iptables -A INPUT -p tcp --sport 10011 -j ACCEPT
+#Start the server
+sh /home/teamspeak/ts3server_startscript.sh start
+
+#Start with Query Password Set
+sh /home/teamspeak/ts3server_startscript.sh start serveradmin_password=QUERY-PASSWORD-HERE
+
+
+#Start on boot
+nano /lib/systemd/system/teamspeak.service
+
+#paste this to the file
+[Unit]
+Description=TeamSpeak 3 Server
+After=network.target
+
+[Service]
+WorkingDirectory=/home/teamspeak/
+User=teamspeak
+Group=teamspeak
+Type=forking
+ExecStart=/home/teamspeak/ts3server_startscript.sh start serveradmin_password=QUERY-PASSWORD-HERE
+ExecStop=/home/teamspeak/ts3server_startscript.sh stop
+PIDFile=/home/teamspeak/ts3server.pid
+RestartSec=15
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+
+#This makes to systemd recognize the file we just created.
+systemctl --system daemon-reload
+#Enable the service.
+systemctl enable teamspeak.service
+#Start the TeamSpeak server.
+systemctl start teamspeak.service
+#Once you've started the server, you can check that it's running with this command.
+systemctl status teamspeak.service
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
