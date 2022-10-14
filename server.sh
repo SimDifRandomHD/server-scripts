0a04c6b081fa89c4a49797959284275fc9d03377
SimDifRandomHD <lachlan@lochstudios.com>
2022-10-14 16:53:32 +1100

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install zip unzip git docker docker.io cockpit -y
+sudo apt-get install zip unzip git -y
+sh -c "$(wget https://raw.githubusercontent.com/SimDifRandomHD/server-scripts/master/terminal.sh -O -)"
